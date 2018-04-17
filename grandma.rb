d8f8082d0496cf2a9ed329b3b4787742a612f74e
mariesonko62@gmail.com <mariesonko62@gmail.com>
2018-04-17 01:38:35 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,18 +1,16 @@
  def speak_to_grandma method(phrase)
-   puts phrase
-   if phrase == "I LOVE YOU GRANDMA!"
+   if phrase == "I LOVE YOU GRANDMA!".upcase
      puts "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == "Hi Nana, how are you?"
+  elsif phrase == "Hi Nana, how are you?".downcase
     puts "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == "Hi!"
+  elsif phrase == "Hi!".downcase
     puts "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == "WHAT DID YOU EAT TODAY"
+  elsif phrase == "WHAT DID YOU EAT TODAY".upcase
     puts "NO, NOT SINCE 1938!"
-  elsif phrase == "WHAT?"
+  else phrase == "WHAT?".upcase 
     puts "NO, NOT SINCE 1938!"
-  else puts "I LOVE YOU TOO PUMPKIN!"
+    
   end
-speak_to_grandma
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
